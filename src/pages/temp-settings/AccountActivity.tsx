407dcdb4df7669e0bdf597f194205d2fdb84c83f
babafemij-k <olaolujkb21@gmail.com>
2022-09-15 15:23:02 +0100

    fix: Added onClick handler to MainAppButton to trigger modal pop-up

--- src/pages/temp-settings/AccountActivity.tsx
+++ src/pages/temp-settings/AccountActivity.tsx
@@ -59,12 +59,10 @@ const AccountActivity = () => {
             reactivation process until at least two hours have passed
           </Text>
         </VStack>
-        <MainAppButton isLoading={false} width={"sm"}>
+        <MainAppButton isLoading={false} width={"sm"} onClick={onOpen}>
           Disable this Account
         </MainAppButton>
       </VStack>
-
-      <Button onClick={onOpen}>Open Modal</Button>
 
       <Modal closeOnOverlayClick={true} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay
