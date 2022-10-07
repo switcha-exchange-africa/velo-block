f520c654853cf422f7b9f9d4000d850a33b26565
damzylance <wasiuadegoke14@gmail.com>
2022-10-07 08:22:23 +0100

    P2P

---
+++ src/pages/p2p/index.tsx
@@ -0,0 +1,290 @@
+import DashboardLayout from "../../layouts/dashboard/DashboardLayout";
+import { useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Image,
+  Flex,
+  Link,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Input,
+  InputGroup,
+  InputRightAddon,
+  Select,
+  Button,
+  Table,
+  TableContainer,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Avatar,
+  Modal,
+  ModalOverlay,
+  ModalHeader,
+  ModalCloseButton,
+  ModalContent,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/react";
+import { CheckCircleIcon } from "@chakra-ui/icons";
+
+function P2P() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <DashboardLayout>
+      <Box
+        background={"#fff"}
+        width={"95%"}
+        height={"100vh"}
+        margin={"20px auto"}
+        padding={["20px"]}
+      >
+        <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
+          <ModalContent maxW="73rem" top={"20rem"} left={"7rem"}>
+            <ModalCloseButton />
+            <ModalBody>
+              <Text fontWeight="bold" mb="1rem">
+                You can scroll the content behind the modal
+              </Text>
+            </ModalBody>
+
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} onClick={onClose}>
+                Close
+              </Button>
+              <Button variant="ghost">Secondary Action</Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+
+        <Flex justifyContent={"space-between"} alignItems="center">
+          <Box display={"flex"} gap="20px" alignItems={"center"}>
+            <Text fontSize={"md"}>Buy</Text>
+            <Image
+              src="/assets/images/separator.png"
+              height={"18px"}
+              alt="separator"
+            />
+            <Text fontSize={"md"}>Sell</Text>
+          </Box>
+          <Link>
+            <Text
+              textDecoration={"underline"}
+              color={"#8B8CA7"}
+              fontSize={"small"}
+            >
+              See all
+            </Text>
+          </Link>
+        </Flex>
+        <Tabs variant="unstyled" mt={["20px"]}>
+          <TabList gap={"60px"}>
+            <Tab
+              _selected={{
+                color: "black",
+                borderBottom: "1px solid #FB5E04",
+              }}
+              padding={0}
+              fontSize={"small"}
+            >
+              BTC
+            </Tab>
+            <Tab
+              _selected={{
+                color: "black",
+                borderBottom: "1px solid #FB5E04",
+              }}
+              padding={0}
+              fontSize={"small"}
+            >
+              ETH
+            </Tab>
+            <Tab
+              _selected={{
+                color: "black",
+                borderBottom: "1px solid #FB5E04",
+              }}
+              padding={0}
+              fontSize={"small"}
+            >
+              USDT
+            </Tab>
+            <Tab
+              _selected={{
+                color: "black",
+                borderBottom: "1px solid #FB5E04",
+              }}
+              padding={0}
+              fontSize={"small"}
+            >
+              USDC
+            </Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel paddingLeft={0}>
+              <Flex justifyContent={"space-between"} alignItems={"center"}>
+                <Flex alignItems={"flex-end"} gap="20px">
+                  <Box>
+                    <Text fontSize={"xs"}>Amount</Text>
+                    <InputGroup size="sm">
+                      <Input
+                        borderTopLeftRadius={"5px"}
+                        borderBottomLeftRadius={"5px"}
+                        placeholder="Enter amount NGN"
+                      />
+                      <InputRightAddon
+                        borderTopRightRadius={"5px"}
+                        borderBottomRightRadius={"5px"}
+                        children="Search"
+                        bg={"none"}
+                        color={"#FB5E04"}
+                      />
+                    </InputGroup>
+                  </Box>
+                  <Box>
+                    <Text fontSize={"xs"}>Fiat</Text>
+                    <Select size={"sm"} defaultValue="NGN" borderRadius={"5px"}>
+                      <option value="NGN">NGN</option>
+                    </Select>
+                  </Box>
+                  <Box>
+                    <Text fontSize={"xs"}>Payment</Text>
+                    <Select
+                      size={"sm"}
+                      defaultValue="NGN"
+                      placeholder="All Payment"
+                      borderRadius={"5px"}
+                    >
+                      <option value="NGN">Bank Transfer</option>
+                    </Select>
+                  </Box>
+                  <Button background={"#FB5E04"} color={"#fff"} size={"sm"}>
+                    Create Ads
+                  </Button>
+                </Flex>
+                <Box
+                  background={"#FFF7F2"}
+                  padding={"5px 20px"}
+                  borderRadius={"5px"}
+                >
+                  <Text>0 Fee</Text>
+                </Box>
+              </Flex>
+              <TableContainer>
+                <Table variant="unstyled">
+                  <Thead>
+                    <Tr fontSize={"sm"} textAlign={"left"} color="#8E9BAE">
+                      <Td>Advertiser</Td>
+                      <Td>Price</Td>
+                      <Td>Available</Td>
+                      <Td>Limit</Td>
+                      <Td>Payment</Td>
+                      <Td display={"flex"} gap="5px">
+                        <Text>Trade</Text> <Text>0Fee</Text>
+                      </Td>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    <Tr borderTop={"1px solid #E2E8F0"}>
+                      <Td>
+                        <Box display={"flex"} alignItems={"center"} gap="5px">
+                          <Avatar
+                            size={"md"}
+                            background={"#FB5E04"}
+                            name="Maximus"
+                          ></Avatar>
+                          <Box>
+                            <Box
+                              display={"flex"}
+                              alignItems={"center"}
+                              gap="3px"
+                            >
+                              <Text fontSize={"sm"}>Maximus</Text>
+                              <CheckCircleIcon
+                                color={"#22C36B"}
+                                w={"10px"}
+                                h={"10px"}
+                              />
+                            </Box>
+                            <Box
+                              display={"flex"}
+                              alignItems={"center"}
+                              gap="3px"
+                              fontSize={"xs"}
+                              color="#8E9BAE"
+                            >
+                              <Text>1534 orders</Text>|
+                              <Text>92.19% completion</Text>
+                            </Box>
+                          </Box>
+                        </Box>
+                      </Td>
+                      <Td>
+                        <Box display={"flex"} alignItems="flex-end">
+                          <Text fontSize={"sm"}>580.50</Text>
+                          <Text fontSize={"xs"}> NGN</Text>
+                        </Box>
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontSize={"sm"}>1,121.0045</Text>
+                        </Box>
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Text fontSize={"sm"}>200 - 500 USD</Text>
+                        </Box>
+                      </Td>
+                      <Td>
+                        <Text
+                          fontSize={"10px"}
+                          textAlign={"center"}
+                          background={"#FFF7F2"}
+                          color={"#FB5E04"}
+                          borderRadius={"3px"}
+                        >
+                          Bank Transfer
+                        </Text>
+                      </Td>
+                      <Td>
+                        <Button
+                          width={"100px"}
+                          fontSize={"sm"}
+                          background={"#22C36B"}
+                          textAlign={"center"}
+                          color="#fff"
+                          borderRadius={"3px"}
+                          onClick={onOpen}
+                        >
+                          Buy BTC
+                        </Button>
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </TabPanel>
+            <TabPanel>
+              <p>ETH</p>
+            </TabPanel>
+            <TabPanel>
+              <p>USDT</p>
+            </TabPanel>
+            <TabPanel>
+              <p>USDC</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </DashboardLayout>
+  );
+}
+
+export default P2P;
