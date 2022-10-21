cddfac5dc0ce7c558881375ee63dece8c1744f2a
otinomz <awesomeotinomz@gmail.com>
2022-10-21 22:36:49 +0100

    feat: table component created

---
+++ src/components/table/TableContainer.tsx
@@ -0,0 +1,239 @@
+import Verified from "../../../public/assets/svgs/verified.svg"
+import Line from "../../../public/assets/svgs/line.svg"
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  HStack,
+  Box,
+  Flex,
+  Text,
+  Button,
+} from '@chakra-ui/react'
+import Image from 'next/image'
+
+const TableComponent = () => {
+    return (
+        <TableContainer>
+            <Table variant='simple'>
+                <Thead>
+                    <Tr>
+                        <Th>Advertiser</Th>
+                        <Th>Price</Th>
+                        <Th>Available</Th>
+                        <Th>Limit</Th>
+                        <Th>Payment</Th>
+                        <Th>Trade</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    <Tr>
+                        <Td>
+                            <HStack>
+                                <Box bg="#FB5E04" color="white" padding="9px 13px" fontWeight="bold" borderRadius="50%" fontSize="14px">M</Box>
+                                <HStack flexDirection="column"  alignItems="flex-start" >
+                                    <HStack>
+                                        <Text fontSize="14px" fontWeight="400" color="#FB5E04">Maximus</Text>
+                                        <Flex>
+                                            <Image src={Verified} alt="verified icon" />
+                                        </Flex>
+                                    </HStack>
+                                    <HStack color="#8E9BAE" fontSize="12px">
+                                        <Text ml="-8px" >1534 orders</Text>
+                                        <Box>
+                                            <Image src={Line} alt="line division" />
+                                        </Box>
+                                        <Text>192.19% completion</Text>
+                                    </HStack>
+                                </HStack>
+                                
+                            </HStack>
+                        </Td>
+                        <Td>
+                            <Text fontSize="14px">724.62 <Text as="span" fontSize="10px">NGN</Text></Text>
+                        </Td>
+                        <Td fontSize="14px">0.00346</Td>
+                        <Td fontSize="14px">200 - 500 USD</Td>
+                        <Td fontSize="14px">
+                            <Button fontSize="14px" fontWeight="200"  color="#FB5E04" bg="#FFF7F2" borderRadius="2px" p="2px 5px">Bank Transfer</Button>
+    
+                        </Td>
+                        <Td>
+                            <Button color="white" fontWeight="bold" bg="#22C36B">
+                                Buy BTC
+                            </Button>
+                            
+                        </Td>
+                    </Tr>
+                </Tbody>
+                <Tbody>
+                    <Tr>
+                        <Td>
+                            <HStack>
+                                <Box bg="#FB5E04" color="white" padding="9px 13px" fontWeight="bold" borderRadius="50%" fontSize="14px">M</Box>
+                                <HStack flexDirection="column"  alignItems="flex-start" >
+                                    <HStack>
+                                        <Text fontSize="14px" fontWeight="400" color="#FB5E04">Maximus</Text>
+                                        <Flex>
+                                            <Image src={Verified} alt="verified icon" />
+                                        </Flex>
+                                    </HStack>
+                                    <HStack color="#8E9BAE" fontSize="12px">
+                                        <Text ml="-8px" >1534 orders</Text>
+                                        <Box>
+                                            <Image src={Line} alt="line division" />
+                                        </Box>
+                                        <Text>192.19% completion</Text>
+                                    </HStack>
+                                </HStack>
+                                
+                            </HStack>
+                        </Td>
+                        <Td>
+                            <Text fontSize="14px">724.62 <Text as="span" fontSize="10px">NGN</Text></Text>
+                        </Td>
+                        <Td fontSize="14px">0.00346</Td>
+                        <Td fontSize="14px">200 - 500 USD</Td>
+                        <Td fontSize="14px">
+                            <Button fontSize="14px" fontWeight="200"  color="#FB5E04" bg="#FFF7F2" borderRadius="2px" p="2px 5px">Bank Transfer</Button>
+    
+                        </Td>
+                        <Td>
+                            <Button color="white" fontWeight="bold" bg="#22C36B">
+                                Buy BTC
+                            </Button>
+                            
+                        </Td>
+                    </Tr>
+                </Tbody>
+
+                <Tbody>
+                    <Tr>
+                        <Td>
+                            <HStack>
+                                <Box bg="#FB5E04" color="white" padding="9px 13px" fontWeight="bold" borderRadius="50%" fontSize="14px">M</Box>
+                                <HStack flexDirection="column"  alignItems="flex-start" >
+                                    <HStack>
+                                        <Text fontSize="14px" fontWeight="400" color="#FB5E04">Maximus</Text>
+                                        <Flex>
+                                            <Image src={Verified} alt="verified icon" />
+                                        </Flex>
+                                    </HStack>
+                                    <HStack color="#8E9BAE" fontSize="12px">
+                                        <Text ml="-8px" >1534 orders</Text>
+                                        <Box>
+                                            <Image src={Line} alt="line division" />
+                                        </Box>
+                                        <Text>192.19% completion</Text>
+                                    </HStack>
+                                </HStack>
+                                
+                            </HStack>
+                        </Td>
+                        <Td>
+                            <Text fontSize="14px">724.62 <Text as="span" fontSize="10px">NGN</Text></Text>
+                        </Td>
+                        <Td fontSize="14px">0.00346</Td>
+                        <Td fontSize="14px">200 - 500 USD</Td>
+                        <Td fontSize="14px">
+                            <Button fontSize="14px" fontWeight="200"  color="#FB5E04" bg="#FFF7F2" borderRadius="2px" p="2px 5px">Bank Transfer</Button>
+    
+                        </Td>
+                        <Td>
+                            <Button color="white" fontWeight="bold" bg="#22C36B">
+                                Buy BTC
+                            </Button>
+                            
+                        </Td>
+                    </Tr>
+                </Tbody>
+
+                <Tbody>
+                    <Tr>
+                        <Td>
+                            <HStack>
+                                <Box bg="#FB5E04" color="white" padding="9px 13px" fontWeight="bold" borderRadius="50%" fontSize="14px">M</Box>
+                                <HStack flexDirection="column"  alignItems="flex-start" >
+                                    <HStack>
+                                        <Text fontSize="14px" fontWeight="400" color="#FB5E04">Maximus</Text>
+                                        <Flex>
+                                            <Image src={Verified} alt="verified icon" />
+                                        </Flex>
+                                    </HStack>
+                                    <HStack color="#8E9BAE" fontSize="12px">
+                                        <Text ml="-8px" >1534 orders</Text>
+                                        <Box>
+                                            <Image src={Line} alt="line division" />
+                                        </Box>
+                                        <Text>192.19% completion</Text>
+                                    </HStack>
+                                </HStack>
+                                
+                            </HStack>
+                        </Td>
+                        <Td>
+                            <Text fontSize="14px">724.62 <Text as="span" fontSize="10px">NGN</Text></Text>
+                        </Td>
+                        <Td fontSize="14px">0.00346</Td>
+                        <Td fontSize="14px">200 - 500 USD</Td>
+                        <Td fontSize="14px">
+                            <Button fontSize="14px" fontWeight="200"  color="#FB5E04" bg="#FFF7F2" borderRadius="2px" p="2px 5px">Bank Transfer</Button>
+                        </Td>
+                        <Td>
+                            <Button color="white" fontWeight="bold" bg="#22C36B">
+                                Buy BTC
+                            </Button>
+                            
+                        </Td>
+                    </Tr>
+                </Tbody>
+
+                <Tbody>
+                    <Tr>
+                        <Td>
+                            <HStack>
+                                <Box bg="#FB5E04" color="white" padding="9px 13px" fontWeight="bold" borderRadius="50%" fontSize="14px">M</Box>
+                                <HStack flexDirection="column"  alignItems="flex-start" >
+                                    <HStack>
+                                        <Text fontSize="14px" fontWeight="400" color="#FB5E04">Maximus</Text>
+                                        <Flex>
+                                            <Image src={Verified} alt="verified icon" />
+                                        </Flex>
+                                    </HStack>
+                                    <HStack color="#8E9BAE" fontSize="12px">
+                                        <Text ml="-8px" >1534 orders</Text>
+                                        <Box>
+                                            <Image src={Line} alt="line division" />
+                                        </Box>
+                                        <Text>192.19% completion</Text>
+                                    </HStack>
+                                </HStack>
+                                
+                            </HStack>
+                        </Td>
+                        <Td>
+                            <Text fontSize="14px">724.62 <Text as="span" fontSize="10px">NGN</Text></Text>
+                        </Td>
+                        <Td fontSize="14px">0.00346</Td>
+                        <Td fontSize="14px">200 - 500 USD</Td>
+                        <Td >
+                            <Button fontSize="14px" fontWeight="200"  color="#FB5E04" bg="#FFF7F2" borderRadius="2px" p="2px 5px">Bank Transfer</Button>
+                        </Td>
+                        <Td>
+                            <Button color="white" fontWeight="bold" bg="#22C36B">
+                                Buy BTC
+                            </Button>
+                            
+                        </Td>
+                    </Tr>
+                </Tbody>
+            </Table>
+        </TableContainer>
+    )
+}
+
+export default TableComponent
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,18 +3,23 @@ import React from "react";
 import DashboardLayout from "../../layouts/dashboard/DashboardLayout";
 import { CardData } from "../../utilities/features/data";
 import { MenuItemsCard } from "../../components/dashboard/menuCard/MenuItemsCard";
+import TableComponent from "../../components/table/TableContainer";
 
 const DashboardPage = () => {
+  const minWeightProps = ["140px", "140px", "140px", "0%"]
+  const scrollbarProps = {
+  '::-webkit-scrollbar':{
+    display: ["scroll", "scroll", "scroll", "none"]
+  }}
+
+
   return (
     <DashboardLayout>
       <Box>
         <Heading>Exchange Crypto with <Text as="span" color="#FB5E04">Low Fees</Text></Heading>
         <Text>Convert your crypto within seconds</Text>
       </Box>
-      <HStack mt="48px" maxW="100%" justifyContent="space-between" fontSize="13px" overflowX="scroll" sx={{
-        '::-webkit-scrollbar': {
-          display: ["scroll", "scroll", "scroll", "none"]
-        }}}
+      <HStack mt="48px" spacing="24px" maxW="100%" justifyContent="space-between" fontSize="13px" overflowX="scroll" sx={scrollbarProps}
       >
         {CardData.map((data) => (
           <MenuItemsCard key={data.id}
@@ -26,51 +31,61 @@ const DashboardPage = () => {
         ))}
       </HStack>
       
-      <HStack justifyContent="space-around" mt="48px" w="80%" mx="auto">
-        <Box >
-          <Flex justifyContent="space-between" fontSize="13px">
+      <HStack mb="48px" justifyContent={["space-between", "space-between", "space-between", "space-around"]}  mt="48px" maxW={["100%", "100%", "100%", "85%"]} mx="auto" overflowX="scroll" sx={scrollbarProps}>
+        <Box minW={minWeightProps}>
+          <Flex fontSize="13px">
             <Text>BTC/USDT</Text>
-            <Text color="#22C36B">+0.60%</Text>
+            <Text color="#22C36B" ml="4px">+0.60%</Text>
           </Flex>
           <Heading fontSize="24px" color="#22C36B">
             41,950.87
           </Heading>
         </Box>
         
-        <Box >
-          <Flex justifyContent="space-between" fontSize="13px">
+        <Box minW={minWeightProps}>
+          <Flex fontSize="13px">
             <Text>ETH/USDT</Text>
-            <Text color="#E95455">-0.60%</Text>
+            <Text color="#E95455" ml="4px">-0.60%</Text>
           </Flex>
           <Heading fontSize="24px" color="#E95455">
             2,948.51
           </Heading>
         </Box>
 
-        <Box >
-          <Flex justifyContent="space-between" fontSize="13px">
+        <Box minW={minWeightProps}>
+          <Flex fontSize="13px">
             <Text>BTC/USDT</Text>
-            <Text color="#E95455">-0.60%</Text>
+            <Text color="#E95455" ml="4px">-0.60%</Text>
           </Flex>
           <Heading fontSize="24px" color="#E95455">
             41,950.87
           </Heading>
         </Box>
-
-        <Box >
-          <Flex justifyContent="space-between" fontSize="13px">
+        
+        <Box minW={minWeightProps}>
+          <Flex fontSize="13px">
             <Text>BTC/USDT</Text>
-            <Text color="#22C36B">+0.60%</Text>
+            <Text color="#22C36B" ml="4px">+0.60%</Text>
           </Flex>
           <Heading fontSize="24px" color="#22C36B">
             41,950.87
           </Heading>
         </Box>
 
-        <Box >
-          <Flex justifyContent="space-between" fontSize="13px">
+        <Box minW={minWeightProps}>
+          <Flex fontSize="13px">
             <Text>BTC/USDT</Text>
-            <Text color="#22C36B">+0.60%</Text>
+            <Text color="#22C36B" ml="4px">+0.60%</Text>
+          </Flex>
+          <Heading fontSize="24px" color="#22C36B">
+            41,950.87
+          </Heading>
+        </Box>
+
+        <Box minW={minWeightProps}>
+          <Flex fontSize="13px">
+            <Text>BTC/USDT</Text>
+            <Text color="#22C36B" ml="4px">+0.60%</Text>
           </Flex>
           <Heading fontSize="24px" color="#22C36B">
             41,950.87
@@ -78,6 +93,8 @@ const DashboardPage = () => {
         </Box>
       </HStack>
       
+
+      <TableComponent />
 
     </DashboardLayout>
   );
