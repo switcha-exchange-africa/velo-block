f1174290beb4d53179ce893ad4c28f398ad9eb64
Joshua Obateru <obtjoshua@gmail.com>
2022-11-01 11:00:50 +0100

    fix: rate to 2dp, fee to 0.5%

--- src/components/quick-trade/QuickBuyComponent.tsx
+++ src/components/quick-trade/QuickBuyComponent.tsx
@@ -14,7 +14,7 @@ import RenderCoinsDropdown from '../select/RenderCoinsDropdown';
 
 const QuickBuyComponent = () => {
     const router = useRouter()
-    const { amount, cash, coin, creditCoinAmount, fee, rate } = useAppSelector((state) => state.quickTrade)
+    const { amount, cash, coin, creditCoinAmount, rate } = useAppSelector((state) => state.quickTrade)
     const [creditCoin, setCreditCoin] = useState(coin ?? `BTC`)
     const [debitCoin, setDebitCoin] = useState(cash ?? `NGN`)
     const [amountt, setAmountt] = useState(amount ? `${amount}` : '0')
@@ -92,11 +92,14 @@ const QuickBuyComponent = () => {
                                     </FormControl>
                                 )}
                             </Field>
+                            <Text fontSize={'xs'} color={'textLightColor'} pb={'1'}>Fee : 0.5%</Text>
+                            <Text fontSize={'xs'} color={'textLightColor'} pb={'2'}>Rate per dollar {rate ? parseFloat(rate).toFixed(2) : parseFloat(ratePerDollar?.data?.data?.destinationAmount?.rate).toFixed(2)} {debitCoin}</Text>
 
-                            <Text fontSize={'xs'} color={'textLightColor'} pb={'2'}>Rate per dollar {rate ?? ratePerDollar?.data?.data?.destinationAmount?.rate} {debitCoin}</Text>
-                            <Box py={'2'}></Box>
+                            <Box py={'1'}></Box>
                             <MainAppButton isLoading={isSubmitting} onClick={handleSubmit} backgroundColor={'secondaryColor.900'} >
-                                Buy with {fee ?? calculateQuickBuyFees?.data?.data?.fee} {debitCoin} fee
+                                {/* Buy with {fee ?? calculateQuickBuyFees?.data?.data?.fee} {debitCoin} fee */}
+                                Buy
+
                             </MainAppButton>
 
 
