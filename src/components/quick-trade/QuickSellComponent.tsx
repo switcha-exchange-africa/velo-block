322ec82cb78dc824b6f67bbccdf0c2e97c5e07dd
Joshua Obateru <obtjoshua@gmail.com>
2022-11-04 13:04:22 +0100

    feat: quick sell integration

--- src/components/quick-trade/QuickSellComponent.tsx
+++ src/components/quick-trade/QuickSellComponent.tsx
@@ -2,23 +2,34 @@ import { Box, Flex, FormControl, FormErrorMessage, FormLabel, Input, InputGroup,
 import { Field, Form, Formik } from 'formik';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../helpers/hooks/reduxHooks';
+import { setQuickBuyPayload } from '../../redux/features/quick-trade/quickTradeSlice';
+import { useConvertQuery, useGetCoinsByTypeQuery } from '../../redux/services/buy-sell.service';
 import MainAppButton from '../buttons/MainAppButton';
-import CustomSelectWithIcon from '../select/CustomSelectWithIcon';
+import RenderCoinsDropdown from '../select/RenderCoinsDropdown';
 
-const coinOptions = [{ value: 'BTC', label: 'BTC', imageUrl: '/assets/svgs/BTC.svg', }, { value: 'ETH', label: 'ETH', imageUrl: '/assets/svgs/ETH.svg', }]
-const currencyOptions = [{ value: 'NGN', label: 'NGN', imageUrl: '/assets/svgs/NGN.svg', },]
+
 
 const QuickSellComponent = () => {
     const router = useRouter()
-    const [creditCoin, setCreditCoin] = useState(`${currencyOptions[0].value}`)
-    const [debitCoin, setDebitCoin] = useState(`${coinOptions[0].value}`)
+    const { amount, cash, coin, creditCoinAmount, } = useAppSelector((state) => state.quickTrade)
+    const [creditCoin, setCreditCoin] = useState(cash ?? `NGN`)
+    const [debitCoin, setDebitCoin] = useState(coin ?? `BTC`)
+    const [amountt, setAmountt] = useState(amount ? `${amount}` : '0')
+    const coinsByTypeCrypto: any = useGetCoinsByTypeQuery('crypto')
+    const coinsByTypeFiat: any = useGetCoinsByTypeQuery('fiat')
+
+    const convertFromDebitCoin: any = useConvertQuery({ amount: amountt, source: debitCoin, destination: creditCoin }, { skip: amountt == '0', refetchOnMountOrArgChange: true })
+
+    const dispatch = useAppDispatch()
     return (
         <Flex flexDirection={'column'} p={'8'}>
             <Formik
-                initialValues={{ email: '', password: '' }}
+                initialValues={{ debitCoinValue: amount ?? '', creditCoinValue: creditCoinAmount ?? '' }}
 
                 onSubmit={async (values, { }) => {
                     console.log(values)
+                    dispatch(setQuickBuyPayload({ amount: parseFloat(amountt), creditCoinAmount: convertFromDebitCoin?.data?.data?.destinationAmount?.destinationAmount, fee: '0.5%', cash: creditCoin, coin: debitCoin, rate: 'no rate for now' }))
                     router.push('/quick-trade/confirm-sales')
                 }}
                 validateOnChange
@@ -28,6 +39,7 @@ const QuickSellComponent = () => {
                 {({
                     // handleChange,
                     // handleBlur,
+                    setFieldValue,
                     handleSubmit,
                     isSubmitting,
                     // values
@@ -35,32 +47,43 @@ const QuickSellComponent = () => {
                 }) => (
                     <Form>
                         <VStack w={{ base: 'xs', lg: 'md' }} align='start'>
-                            <Field name='email' >
+                            <Field name='debitCoinValue' >
                                 {({ field, form }: any) => (
-                                    <FormControl isInvalid={form.errors.email && form.touched.email} pt='4'>
-                                        <FormLabel fontSize={'xs'} color={'textLightColor'}>I want to sell</FormLabel>
+                                    <FormControl isInvalid={form.errors.debitCoinValue && form.touched.debitCoinValue} pt='4'>
+                                        <FormLabel fontSize={'xs'} color={'textLightColor'}>I want to pay</FormLabel>
                                         <InputGroup>
-                                            <Input autoComplete='off' variant={'outline'} {...field} />
-                                            <InputRightElement width={'28'} zIndex={'docked'}>
-                                                <CustomSelectWithIcon items={coinOptions} onChange={(selectedValue) => setDebitCoin(selectedValue)} value={debitCoin} />
+                                            <Input autoComplete='off' variant={'outline'} {...field} onChange={(e) => {
+                                                setFieldValue('debitCoinValue', e.target.value);
+                                                setAmountt(e.target.value)
+                                                // alert(amount)
+                                                // setFieldValue('creditCoinValue', e.target.value) 
+                                                !(convertFromDebitCoin.isFetching) && convertFromDebitCoin?.data?.data?.destinationAmount?.destinationAmount && setFieldValue('creditCoinValue', convertFromDebitCoin?.data?.data?.destinationAmount?.destinationAmount)
+
+                                            }} />
+                                            <InputRightElement width='28'  >
+
+                                                {coinsByTypeCrypto?.data?.data && <RenderCoinsDropdown items={coinsByTypeCrypto?.data?.data} onChange={(selectedValue) => setDebitCoin(selectedValue)} value={debitCoin} />}
+
+
                                             </InputRightElement>
                                         </InputGroup>
-                                        <FormErrorMessage>{form.errors.email}</FormErrorMessage>
+                                        <FormErrorMessage>{form.errors.debitCoinValue}</FormErrorMessage>
                                     </FormControl>
                                 )}
                             </Field>
 
-                            <Field name='password' >
-                                {({ field, form }: any) => (
-                                    <FormControl isInvalid={form.errors.password && form.touched.password} py='4'>
-                                        <FormLabel fontSize={'xs'} color={'textLightColor'}>I will receive</FormLabel>
-                                        <InputGroup>
-                                            <Input disabled autoComplete='off' variant={'outline'} {...field} />
-                                            <InputRightElement width={'28'} >
-                                                <CustomSelectWithIcon items={currencyOptions} onChange={(selectedValue) => setCreditCoin(selectedValue)} value={creditCoin} />
-                                            </InputRightElement>
-                                        </InputGroup>
-                                        <FormErrorMessage>{form.errors.password}</FormErrorMessage>
+                            <Field name='creditCoinValue' >
+                                {({ form }: any) => (
+                                    <FormControl isInvalid={form.errors.creditCoinValue && form.touched.creditCoinValue} py='4'>
+                                        <FormLabel fontSize={'xs'} color={'textLightColor'}>To</FormLabel>
+                                        <Flex pl={'4'} w='full' border={'1px'} zIndex={'base'} borderColor={'gray.200'} borderRadius={'8'} justifyContent={'space-between'} alignItems={'center'} ><Text w='full'>{convertFromDebitCoin?.data?.data?.destinationAmount?.destinationAmount?.toLocaleString() ?? creditCoinAmount?.toLocaleString() ?? 0}</Text> {coinsByTypeFiat?.data?.data && <RenderCoinsDropdown items={coinsByTypeFiat?.data?.data} onChange={(selectedValue) => setCreditCoin(selectedValue)} value={creditCoin} />}</Flex>
+                                        {/* <InputGroup>
+                                        <Input disabled autoComplete='off' variant={'outline'} {...field} />
+                                        <InputRightElement width={'40'} >
+                                            {allCoins?.data?.data && <RenderCoinsDropdown items={allCoins?.data?.data} onChange={(selectedValue) => setCreditCoin(selectedValue)} value={creditCoin} />}
+                                        </InputRightElement>
+                                    </InputGroup> */}
+                                        <FormErrorMessage>{form.errors.creditCoinValue}</FormErrorMessage>
                                     </FormControl>
                                 )}
                             </Field>
@@ -69,7 +92,7 @@ const QuickSellComponent = () => {
                             <Text fontSize={'xs'} color={'textLightColor'} pb={'2'}>Fee : 0.5%  </Text>
                             <Box py={'2'}></Box>
                             <MainAppButton isLoading={isSubmitting} onClick={handleSubmit} backgroundColor={'secondary2Color.900'}>
-                                Sell with 0 fee
+                                Sell
                             </MainAppButton>
 
 
--- src/pages/quick-trade/confirm-sales/index.tsx
+++ src/pages/quick-trade/confirm-sales/index.tsx
@@ -4,27 +4,75 @@ import { useRouter } from 'next/router'
 import React from 'react'
 import MainAppButton from '../../../components/buttons/MainAppButton'
 import PaymentMethodComponent from '../../../components/quick-trade/PaymentMethodComponent'
+import appAlert from '../../../helpers/appAlert'
+import { useAppDispatch, useAppSelector } from '../../../helpers/hooks/reduxHooks'
 import DashboardLayout from '../../../layouts/dashboard/DashboardLayout'
+import { setOrderPayload } from '../../../redux/features/quick-trade/quickTradeSlice'
+import { useQuickTradeMutation } from '../../../redux/services/quick-trade.service'
+import Currency from 'react-currency-formatter';
 
 const ConfirmSales = () => {
     const router = useRouter()
+    const { amount, cash, coin, creditCoinAmount, fee, rate } = useAppSelector((state) => state.quickTrade)
+    const dispatch = useAppDispatch()
+
+    React.useEffect(() => {
+        // alert(`${amount}, ${cash}, ${coin}, ${creditCoinAmount}, ${fee}`)
+    }, [amount, cash, coin, creditCoinAmount, fee, rate])
+
+    const [quickTrade, { isLoading }] = useQuickTradeMutation()
+
+    const handleSubmit = async () => {
+        try {
+
+            const response: any = await quickTrade({
+                amount: creditCoinAmount,
+                cash: cash,
+                coin: coin,
+                method: "bank",
+                type: "sell"
+            })
+            if (response?.data?.status == 200) {
+                // alert(JSON.stringify(response?.data?.data))
+                appAlert.success('order created successfully')
+                dispatch(setOrderPayload({ order: response?.data?.data }))
+                const orderId = response?.data?.data?.order?.orderId
+                router.push(`/quick-trade/${orderId}`)
+
+            } else if (response?.data?.status == 401) {
+
+                appAlert.error(`${response?.error?.data?.message}`)
+                // alert(JSON.stringify(res))
+                router.replace('/signin')
+            } else {
+
+                appAlert.error(`${response?.error?.data?.message ?? 'An error Occured'}`)
+            }
+        } catch (error) {
+
+            console.log(error)
+        }
+    }
     return (
         <DashboardLayout title='Confirm Sales'>
             <Flex bg={'mainBGColor'} justifyContent={'center'} alignItems='center' w='full' h={'full'}>
                 <Box bg={'appWhiteColor'} p={'4'}>
                     <Flex flexDirection={'column'}>
                         <Flex alignItems={'center'} pb={'4'}>
-                            <ChevronLeftIcon onClick={() => { router.back() }} />
+                            <ChevronLeftIcon onClick={() => { router.replace({ pathname: '/quick-trade', query: { isBuySelectedProps: false } }) }} />
                             <Text fontSize='lg' as='p' fontWeight={'light'} py={'2'} w={'full'} align={'center'} >Confirm Sales</Text>
                         </Flex>
-                        <Text fontSize='2xl' as='b' w={'full'} align={'center'} >N10,000.00</Text>
-                        <Text fontSize='xs' as='p' fontWeight={'light'} w={'full'} align={'center'} >I will receive 18.18 USDT</Text>
+                        <Text fontSize='2xl' as='b' w={'full'} align={'center'} >{amount} {coin}</Text>
+                        <Text fontSize='xs' as='p' fontWeight={'light'} w={'full'} align={'center'} >I will receive <Currency
+                            quantity={creditCoinAmount}
+                            currency={cash}
+                        /></Text>
                         <Text fontSize='xs' as='p' fontWeight={'semibold'} color={'textLightColor'} w={'full'} align={'left'} pt={'8'} pb={'1'}>Select payment method</Text>
                         <PaymentMethodComponent borderColor={'primaryColor.900'} label={'Bank Transfer'} rate={'550.67'} />
                         <Box py={'2'}></Box>
                         <PaymentMethodComponent borderColor={'paymentMethodColor2'} label={'Switch NG Wallet'} rate={'550.67'} disabled />
                         <Box p={'12'}></Box>
-                        <MainAppButton isLoading={false} backgroundColor={'secondary2Color.900'} >
+                        <MainAppButton isLoading={isLoading} onClick={() => handleSubmit()} backgroundColor={'secondary2Color.900'} >
                             Confirm Purchase
                         </MainAppButton>
 
