ed2c6c9c324f161cbd3ec33a9a59e4eb833b6e61
otinomz <awesomeotinomz@gmail.com>
2022-11-07 21:03:39 +0100

    feat: getting the first values

--- src/components/p2p/steps/BuyStepOne.tsx
+++ src/components/p2p/steps/BuyStepOne.tsx
@@ -1,5 +1,4 @@
-import { Box, Button, Flex, HStack, Tab, TabList, TabPanel, TabPanels, Tabs, Text, VStack  } from '@chakra-ui/react';
-// import { useGetAllCoinsQuery } from '../../../redux/services/buy-sell.service';
+import { Box, Button, Flex, FormControl, HStack, Tab, TabList, TabPanel, TabPanels, Tabs, Text, VStack  } from '@chakra-ui/react';
 import Asset from '../radioGroup/Asset';
 import InputCounter from '../radioGroup/Counter';
 import PriceType from '../radioGroup/PriceType';
@@ -15,73 +14,98 @@ interface BuyStepProps {
 const BuyStepOne = ({ handleNextStep }: BuyStepProps) => {
     
     const [value, setValue] = useState('0')
+    const [asset, setAsset] = useState('BTC')
+    const [withCash, setWithCash] = useState('NGN')
+    const [priceType, setPriceType] = useState('Fixed')
     const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => setValue(event.target.value)
     
+
+
+    const handleSubmit = (e:any) => {
+        e.preventDefault()
+        const data = {
+            asset,
+            withCash,
+            priceType,
+            value
+        }
+
+        console.log("first data ", data)
+
+        handleNextStep()   
+        
+    }
     
+
+
     return (
-        <Box position="relative"  px="0">
-            <Box mt="50px" fontFamily={"Open Sans"} bg="white" >
-            <Tabs variant='unstyled' w="100%">
-                <TabList px={["15px", "10px", "0px"]}>
-                    <HStack w="100%" alignItems="center" >
-                        <Tab _selected={{ color: '#000000', bg: '#F5f5f5' }} width="50%" py={"24px"} fontSize={"16px"} fontWeight={"600"}>I want to Buy</Tab>
-                        <Tab _selected={{ color: '#000000', bg: '#F5F5F5' }} width="50%" py={"24px"} fontSize={"16px"} fontWeight={"600"}>I want to Sell</Tab>
-                    </HStack>
-                </TabList>
-                <TabPanels>
-                    <TabPanel px={["15px", "10px", "60px"]} pb="70px">
-                        {/* asset radio group imported here*/}
-                        <Asset />
-                        {/* with Cash group imported here */}
-                        <WithCash/>
+        <form style={{position: "relative"}} onSubmit={handleSubmit}>
+            <FormControl isRequired>
+                <Box mt="50px" fontFamily={"Open Sans"} bg="white" >
+                    <Tabs variant='unstyled' w="100%">
+                        <TabList px={["15px", "10px", "0px"]}>
+                            <HStack w="100%" alignItems="center" >
+                                <Tab _selected={{ color: '#000000', bg: '#F5f5f5' }} width="50%" py={"24px"} fontSize={"16px"} fontWeight={"600"}>I want to Buy</Tab>
+                                <Tab _selected={{ color: '#000000', bg: '#F5F5F5' }} width="50%" py={"24px"} fontSize={"16px"} fontWeight={"600"}>I want to Sell</Tab>
+                            </HStack>
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel px={["15px", "10px", "60px"]} pb="70px">
+                            
+                                    {/* asset radio group imported here*/}
+                                    <Asset asset={asset} setAsset={setAsset} />
+                                    {/* with Cash group imported here */}
+                                    <WithCash withCash={withCash} setWithCash={setWithCash}/>
 
-                        <HStack my="20px" gap={"50px"}>
-                            <VStack alignItems={"flex-start"}>
-                                <Text color={"#8E9BAE"} fontFamily={"Open Sans"} fontWeight={"600"} fontSize={"14px"}>Your Price</Text>
-                                <Text fontSize="24px" fontWeight={"600"} fontFamily={"Open Sans"}>₦{value.toLocaleString()}</Text>
-                            </VStack>
-                            <VStack alignItems={"flex-start"}>
-                                <Text color={"#8E9BAE"} fontFamily={"Open Sans"} fontWeight={"600"} fontSize={"14px"}>
-                                    Highest Order Price
-                                    <InfoOutlineIcon
-                                        ml="5px"
-                                        color={"#8E9BAE"}
-                                        w={"10px"}
-                                        h={"10px"}
-                                    />    
-                                </Text>
-                                <Text fontSize="24px" fontWeight={"600"} fontFamily={"Open Sans"}>₦570.47</Text>
-                            </VStack>
-                        </HStack>
+                                    <HStack my="20px" gap={"50px"}>
+                                        <VStack alignItems={"flex-start"}>
+                                            <Text color={"#8E9BAE"} fontFamily={"Open Sans"} fontWeight={"600"} fontSize={"14px"}>Your Price</Text>
+                                            <Text fontSize="24px" fontWeight={"600"} fontFamily={"Open Sans"}>₦{value.toLocaleString()}</Text>
+                                        </VStack>
+                                        <VStack alignItems={"flex-start"}>
+                                            <Text color={"#8E9BAE"} fontFamily={"Open Sans"} fontWeight={"600"} fontSize={"14px"}>
+                                                Highest Order Price
+                                                <InfoOutlineIcon
+                                                    ml="5px"
+                                                    color={"#8E9BAE"}
+                                                    w={"10px"}
+                                                    h={"10px"}
+                                                />    
+                                            </Text>
+                                            <Text fontSize="24px" fontWeight={"600"} fontFamily={"Open Sans"}>₦570.47</Text>
+                                        </VStack>
+                                    </HStack>
 
-                        {/* Price type radio group imported here */}
-                        <PriceType/>                            
+                                    {/* Price type radio group imported here */}
+                                <PriceType priceType={priceType} setPriceType={setPriceType} />                            
 
-                        {/* Floating Price Margin */}
-                        <InputCounter value={value} handleChange={handleChange} />
+                                    {/* Floating Price Margin */}
+                                    <InputCounter value={value} handleChange={handleChange} />
 
-                        <Flex  bottom={"0px"} p={"12px"} w={"100%"} bg="white" mt="50px" boxShadow={"0px -4px 11px rgba(0, 0, 0, 0.05)"} display={["flex", "flex", "none"]}>
-                            <Button borderRadius={"5px"} bg={"#FB5E04"} color={"white"} p={"11px 44px"} fontSize={"14px"} flex="1" onClick={handleNextStep}>
-                                Next
-                            </Button>
-                        </Flex>
-                        
-                    </TabPanel>
+                                    <Flex  bottom={"0px"} p={"12px"} w={"100%"} bg="white" mt="50px" boxShadow={"0px -4px 11px rgba(0, 0, 0, 0.05)"} display={["flex", "flex", "none"]}>
+                                        <Button borderRadius={"5px"} bg={"#FB5E04"} color={"white"} p={"11px 44px"} type="submit" fontSize={"14px"} flex="1" >
+                                            Next
+                                        </Button>
+                                    </Flex>
+                                
+                            </TabPanel>
 
 
 
-                    <TabPanel>
-                        I want to sell Tab
-                    </TabPanel>
-                </TabPanels>
-            </Tabs>
-            </Box>
-            <Flex justifyContent={"flex-end"} left={"17%"} bottom={"0px"} p={"24px"} w={"82%"} bg="#FFFFFF" position="fixed" boxShadow={"0px -4px 11px rgba(0, 0, 0, 0.05)"} zIndex="20" display={["none", "none", "flex"]}>
-                <Button borderRadius={"5px"} bg={"#FB5E04"} color={"white"} p={"11px 44px"} fontSize={"14px"} onClick={handleNextStep}>
-                    Next
-                </Button>
-            </Flex> 
-        </Box>
+                            <TabPanel>
+                                I want to sell Tab
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+                </Box>
+                <Flex justifyContent={"flex-end"} left={"17%"} bottom={"0px"} p={"24px"} w={"82%"} bg="#FFFFFF" position="fixed" boxShadow={"0px -4px 11px rgba(0, 0, 0, 0.05)"} zIndex="20" display={["none", "none", "flex"]}>
+                    <Button borderRadius={"5px"} bg={"#FB5E04"} color={"white"} p={"11px 44px"} fontSize={"14px"} type="submit" >
+                        Next
+                    </Button>
+                </Flex>
+            </FormControl>
+             
+        </form>
     )
 }
 
