0c54d8e0fb3504f71f2ee4926406f94e0ae09d6b
otinomz <awesomeotinomz@gmail.com>
2022-11-11 00:58:57 +0100

    fix: coin values instead of fait

--- src/redux/services/p2p-ads.service.ts
+++ src/redux/services/p2p-ads.service.ts
@@ -17,60 +17,6 @@ export const adsOrdersApi = baseApi.injectEndpoints({
             },
         }),
 
-        getBuyAdsUSDT: builder.query<any, any>({
-            query: ({arg, pageNumber}) => `${endpoints.P2P_BUY_ADS_URL}=${arg}&perpage=5&page=${pageNumber}`,
-            transformResponse: (responseData: any) => {
-                return responseData;
-            },
-        }),
-        getBuyAdsBTC: builder.query<any, any>({
-            query: ({arg, pageNumber}) => `${endpoints.P2P_BUY_ADS_URL}=${arg}&perpage=5&page=${pageNumber}`,
-            transformResponse: (responseData: any) => {
-                return responseData;
-            },
-        }),
-        getBuyAdsETH: builder.query<any, any>({
-            query: ({arg, pageNumber}) => `${endpoints.P2P_BUY_ADS_URL}=${arg}&perpage=5&page=${pageNumber}`,
-            transformResponse: (responseData: any) => {
-                return responseData;
-            },
-        }),
-        getBuyAdsUSDC: builder.query<any, any>({
-            query: ({arg, pageNumber}) => `${endpoints.P2P_BUY_ADS_URL}=${arg}&perpage=5&page=${pageNumber}`,
-            transformResponse: (responseData: any) => {
-                return responseData;
-            },
-        }),
-
-
-        // sell section
-        getSellAdsUSDT: builder.query<any, any>({
-            query: ({arg, pageNumber}) => `${endpoints.P2P_SELL_ADS_URL}=${arg}&perpage=5&page=${pageNumber}`,
-            transformResponse: (responseData: any) => {
-                return responseData;
-            },
-        }),
-        getSellAdsBTC: builder.query<any, any>({
-            query: ({arg, pageNumber}) => `${endpoints.P2P_SELL_ADS_URL}=${arg}&perpage=5&page=${pageNumber}`,
-            transformResponse: (responseData: any) => {
-                return responseData;
-            },
-        }),
-        getSellAdsETH: builder.query<any, any>({
-            query: ({arg, pageNumber}) => `${endpoints.P2P_SELL_ADS_URL}=${arg}&perpage=5&page=${pageNumber}`,
-            transformResponse: (responseData: any) => {
-                return responseData;
-            },
-        }),
-        getSellAdsUSDC: builder.query<any, any>({
-            query: ({arg, pageNumber}) => `${endpoints.P2P_SELL_ADS_URL}=${arg}&perpage=5&page=${pageNumber}`,
-            transformResponse: (responseData: any) => {
-                return responseData;
-            },
-        }),
-
-
-
         createBuyAds: builder.mutation<any, any>({
             query: (body) => {
                 return {
@@ -90,15 +36,5 @@ export const adsOrdersApi = baseApi.injectEndpoints({
 export const {
     useGetBuyAdsQuery,
     useGetSellAdsQuery,
-    useGetBuyAdsUSDTQuery,
-    useGetBuyAdsBTCQuery,
-    useGetBuyAdsETHQuery,
-    useGetBuyAdsUSDCQuery,
-
-    useGetSellAdsUSDTQuery,
-    useGetSellAdsBTCQuery,
-    useGetSellAdsETHQuery,
-    useGetSellAdsUSDCQuery,
-
     useCreateBuyAdsMutation
 } = adsOrdersApi
