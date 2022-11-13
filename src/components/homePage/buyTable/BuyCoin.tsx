27547cc2d6914afd6d648e2978c1953f6415dbfc
otinomz <awesomeotinomz@gmail.com>
2022-11-13 23:46:42 +0100

    chore: pagination refactor completed

--- src/components/homePage/buyTable/BuyCoin.tsx
+++ src/components/homePage/buyTable/BuyCoin.tsx
@@ -4,41 +4,23 @@ import {useGetBuyAdsQuery} from "../../../redux/services/p2p-ads.service";
 import { P2pAdsComponentProps } from '../../../interfaces/p2p-ads/P2pAdsComponent';
 
 
-const BuyCoin = ({
-    pageNumber,
-    secondPageNumber,
-    thirdPageNumber,
-    fourthPageNumber,
-    fifthPageNumber,
-    
-    handlePreviousPage,
-    handleSecondPreviousPage,
-    handleThirdPreviousPage,
-    handleFourthPreviousPage,
-    handleFifthPreviousPage,
-    
-    handleNextPage,
-    handleSecondNextPage,
-    handleThirdNextPage,
-    handleFourthNextPage,
-    handleFifthNextPage
-}: P2pAdsComponentProps) => {
-    const { data:usdt } = useGetBuyAdsQuery({arg: "USDT", pageNumber: `${thirdPageNumber}`})
-    const { data:usdc } = useGetBuyAdsQuery({arg: "USDC", pageNumber: `${fourthPageNumber}`})
-    const { data:eth } = useGetBuyAdsQuery({arg: "ETH", pageNumber: `${secondPageNumber}`})
+const BuyCoin = ({ pageNumber, handlePreviousPage, handleNextPage, handlePageReset}: P2pAdsComponentProps) => {
+    const { data:usdt } = useGetBuyAdsQuery({arg: "USDT", pageNumber: `${pageNumber}`})
+    const { data:usdc } = useGetBuyAdsQuery({arg: "USDC", pageNumber: `${pageNumber}`})
+    const { data:eth } = useGetBuyAdsQuery({arg: "ETH", pageNumber: `${pageNumber}`})
     const { data:btc } = useGetBuyAdsQuery({arg: "BTC", pageNumber: `${pageNumber}`})
-    const { data:usdt_tron } = useGetBuyAdsQuery({arg: "USDT-TRON", pageNumber: `${fifthPageNumber}`})
-    
+    const { data:usdt_tron } = useGetBuyAdsQuery({arg: "USDT_TRON", pageNumber: `${pageNumber}`})
+
 
 
     return (
         <Tabs variant='unstyled'>
             <TabList gap={"36px"} px={["0", "0px", "28px", "28px"]}>
-                <Tab p={0} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">BTC</Tab>
-                <Tab p={0} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">ETH</Tab>
-                <Tab p={0} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">USDT</Tab>
-                <Tab p={0} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">USDC</Tab>
-                <Tab p={0} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">USDT-TRON</Tab>
+                <Tab p={0} onClick={handlePageReset} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">BTC</Tab>
+                <Tab p={0} onClick={handlePageReset} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">ETH</Tab>
+                <Tab p={0} onClick={handlePageReset} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">USDT</Tab>
+                <Tab p={0} onClick={handlePageReset} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">USDC</Tab>
+                <Tab p={0} onClick={handlePageReset} _selected={{ color: "#000000",  borderBottom: "1px solid #FB5E04" }} fontSize="small">USDT-TRON</Tab>
             </TabList>                
             
             <Box background="#E2E8F0" height="0.1px" width={["100%", "100%", "97%"]} m="14px auto 8px"></Box>
@@ -65,8 +47,8 @@ const BuyCoin = ({
                             buttonTitle="Buy ETH"
                             backgroundColor="#22C36B"
                             apiData={eth}
-                            handlePreviousPage = { handleSecondPreviousPage }
-                            handleNextPage={handleSecondNextPage}
+                            handlePreviousPage = { handlePreviousPage }
+                            handleNextPage={handleNextPage}
                         />      
                     ) : "NO BUY ADS YET"}
                 </TabPanel>
@@ -78,8 +60,8 @@ const BuyCoin = ({
                             buttonTitle="Buy USDT"
                             backgroundColor="#22C36B"
                             apiData={usdt}
-                            handlePreviousPage = { handleThirdPreviousPage }
-                            handleNextPage={handleThirdNextPage}
+                            handlePreviousPage = { handlePreviousPage }
+                            handleNextPage={handleNextPage}
                         />      
                     ) : "NO BUY ADS YET"}           
                 </TabPanel>
@@ -91,8 +73,8 @@ const BuyCoin = ({
                             buttonTitle="Buy USDC"
                             backgroundColor="#22C36B"
                             apiData={usdc}
-                            handlePreviousPage = { handleFourthPreviousPage }
-                            handleNextPage={handleFourthNextPage}
+                            handlePreviousPage = { handlePreviousPage }
+                            handleNextPage={handleNextPage}
                         />      
                     ) : "NO BUY ADS YET"}              
                 </TabPanel>
@@ -104,8 +86,8 @@ const BuyCoin = ({
                             buttonTitle="Buy USDT-TRON"
                             backgroundColor="#22C36B"
                             apiData={usdt_tron}
-                            handlePreviousPage = { handleFifthPreviousPage }
-                            handleNextPage={handleFifthNextPage}
+                            handlePreviousPage = { handlePreviousPage }
+                            handleNextPage={handleNextPage}
                         />      
                     ) : "NO BUY ADS YET"}              
                 </TabPanel>
