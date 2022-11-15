9491055b76f7f9378c1fcdcd6e238c35933ae2ac
otinomz <awesomeotinomz@gmail.com>
2022-11-15 04:28:13 +0100

    chore: settings ui cleanup

--- src/pages/settings/profile/index.tsx
+++ src/pages/settings/profile/index.tsx
@@ -1,7 +1,7 @@
 import { ArrowBackIcon } from "@chakra-ui/icons"
 import {
   Box, Button, Flex, Heading,
-  HStack, Input, Show, Text,
+  Input, Show, Text,
   VStack
 } from '@chakra-ui/react'
 import { useRouter } from 'next/router'
@@ -58,86 +58,107 @@ const Profile = () => {
           </Flex>
         </Show>
 
-        <Box px={{ md: '0', base: '4' }} pt={{ md: '0', base: '12' }}>
-          <Box background={'#FFFFFF'}
-            width={{ lg: "60%", base: '100%' }}
+        <Box px={{ md: '0', base: '4' }} pt={{ md: '0', base: '12' }} >
+          <Box 
+            background={'#FFFFFF'}
+            width={{ lg: "70%", base: '100%' }}
             justifyContent={"space-between"}
-            py={"20px"}>
+            py={"20px"}
+          >
+            
 
-            <Flex width={'100%'}>
+            <Flex width={'100%'} mb="24px"  alignItems="center">
               <Text
-                pl={{ md: '2rem', base: '4' }}
-                pt={'0.5rem'}
                 w={{ base: 'fit-content', md: '60' }}
                 pr={{ base: '5', md: '0' }}
                 fontSize={{ base: 'sm', lg: 'md' }}
-              >Email
+                pl="20px"
+              >
+                Email
               </Text>
-              <Input placeholder='Olumideoyeleye@gmail.com'
-                // ml={'7rem'}
+              <Input
+                placeholder='Olumideoyeleye@gmail.com'
+                mr={'1rem'}
+              />
+            </Flex>
+
+            <Flex width={'100%'} mb="24px" alignItems="center">
+              <Text
+                w={{ base: 'fit-content', md: '60' }}
+                pr={{ base: '5', md: '0' }}
+                fontSize={{ base: 'sm', lg: 'md' }}
+                pl="20px"
+              >
+                Username
+              </Text>
+              <Input placeholder='Eclusive'
+              
                 mr={'1rem'}
 
               />
             </Flex>
 
-            <HStack mt={'2rem'}>
-
-              <Flex width={'100%'}>
-                <Text
-                  pl={{ md: '2rem', base: '4' }}
-                  pt={'0.5rem'} w={{ base: 'fit-content', md: '60' }}
-                  pr={{ base: '5', md: '0' }} fontSize={{ base: 'sm', lg: 'md' }}>Username
-                </Text>
-                <Input placeholder='Eclusive'
-                  // ml={'5rem'}
-                  mr={'1rem'}
-
-                />
-
-
-              </Flex>
-            </HStack>
-
-            <HStack mt={'2rem'}>
-              <Flex width={'100%'}>
-                <Text
-                  pl={{ md: '2rem', base: '4' }}
-                  pt={'0.5rem'} w={{ base: 'fit-content', md: '60' }}
-                  pr={{ base: '5', md: '0' }} fontSize={{ base: 'sm', lg: 'md' }}>Name
-                </Text>
-                <Input placeholder='Temitope'
-                  mr={'1rem'}
-                />
-
-
-              </Flex>
-            </HStack>
-
-            <Flex w={'full'} mt='10' justifyContent={{ base: 'space-between', md: 'start' }}>
+            <Flex width={'100%'} mb="24px"  alignItems="center">
               <Text
-                pl={{ md: '2rem', base: '4' }} w={{ base: 'fit-content', md: '48' }}
-                pr={{ base: '5', md: '0' }} fontSize={{ base: 'sm', lg: 'md' }}>Phone number</Text>
-              <Flex flexDirection={'column'} alignItems={{ base: 'end', md: 'start' }} pr='4'>
+                w={{ base: 'fit-content', md: '60' }}
+                pr={{ base: '5', md: '0' }}
+                fontSize={{ base: 'sm', lg: 'md' }}
+                pl="20px"
+              >
+                Name
+              </Text>
+              <Input placeholder='Temitope'
+                mr={'1rem'}
+              />
+            </Flex>
+
+            <Flex w={'full'} mb="24px" justifyContent={{ base: 'space-between', md: 'start' }} alignItems={"center"}>
+              <Text
+                w={{ base: '60', md: '60' }}
+                pr={{ base: '5', md: '0' }}
+                fontSize={{ base: 'sm', lg: 'md' }}
+                pl="20px"
+              >
+                Phone Number
+              </Text>
+              <Flex flexDirection={{ base: 'column', md: 'row' }}  w="100%" alignItems={{ base: 'end', md: 'start' }} justifyContent={"space-between"} pr='4' fontSize={"14px"}>
                 <Text>***176</Text>
                 <Text
-                  color={'#FB5E04'} fontSize={{ base: 'sm', lg: 'md' }}>Change phone number</Text>
+                  color={'#FB5E04'} fontSize={{ base: 'sm', lg: '14px' }}>Change phone number</Text>
               </Flex>
             </Flex>
 
-            <Flex justifyContent={'space-between'} alignItems={'start'} mt={'2rem'} px={{ md: '8', base: '4' }}>
-
-              <Text fontSize={{ base: 'sm', lg: 'md' }}
-              >Account Status</Text>
-
-              <Flex flexDirection={{ base: 'column', md: 'row' }} justifyContent={'space-between'} alignItems={{ base: 'end', md: 'center' }} w={{ md: '69%', base: '30' }}>
-                <Text fontSize={{ base: 'sm', lg: 'md' }} pb={{ base: '2', md: '0' }}>Level 1 Verified</Text>
+            <Flex w={'full'}  justifyContent={{ base: 'space-between', md: 'start' }} mb={"24px"} alignItems={"center"}>
+              <Text
+                w={{ base: '60', md: '60' }}
+                pr={{ base: '5', md: '0' }}
+                fontSize={{ base: 'sm', lg: 'md' }}
+                pl="20px"
+              >
+                Account Status
+              </Text>
+              <Flex flexDirection={{ base: 'column', md: 'row' }}  w="100%" alignItems={{ base: 'end', md: 'center' }} justifyContent={"space-between"} pr='4' fontSize={"14px"}>
+                <Text m={{ base: '2px', md: '0' }}>Level 1 Verified</Text>
                 <SettingsButton onClick={() => Router.push('/settings/profile/verification')}>Upgrade Verification</SettingsButton>
               </Flex>
-
             </Flex>
 
-            <Flex mt={{ md: '2rem', base: '12' }} justifyContent={{ md: 'end', base: 'start' }} px={{ md: '8', base: '4' }}>
-              <MainAppButton isLoading={false} size={{ base: "sm", md: 'md' }} width={{ base: '40%', md: '35%' }}>Create Ads</MainAppButton>
+            <Flex w={'full'}   justifyContent={{ base: 'space-between', md: 'space-between' }} mb={"24px"} alignItems={"center"}>
+              <Text
+                w={{ base: '60', md: '60' }}
+                pr={{ base: '5', md: '0' }}
+                fontSize={{ base: 'sm', lg: 'md' }}
+                pl="20px"
+              >
+                Bank Accounts
+              </Text>
+              <Box mr={'1rem'} textAlign={'center'} border={" 1px solid #fb5e04"} color={"#333333"} px={{ base: '2', lg: '4' }} py={{ base: '1', lg: '2' }} background={"transparent"} fontSize={{ base: 'xs', lg: '14px' }} cursor={'pointer'}>
+                Manage accounts
+              </Box>
+            </Flex>
+            
+            <Flex mt={{ md: '2rem', base: '12' }} justifyContent={{ md: 'end', base: 'start' }} px={{ md: '24px', base: '4' }}>
+              <MainAppButton isLoading={false} size={{ base: "sm", md: 'md' }} width={{ base: '40%', md: '35%' }}>Save</MainAppButton>
             </Flex>
           </Box>
 
