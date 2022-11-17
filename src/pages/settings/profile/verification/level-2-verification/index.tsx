2fb02332724863e02412623798c8e7fdfca9d035
otinomz <awesomeotinomz@gmail.com>
2022-11-17 14:39:25 +0100

    feat: sending file to digital ocean first trial

--- src/pages/settings/profile/verification/level-2-verification/index.tsx
+++ src/pages/settings/profile/verification/level-2-verification/index.tsx
@@ -41,45 +41,7 @@ const Level2Verification = () => {
     const Router = useRouter()
 
     // Step 3: Define the parameters for the object you want to upload.
-    const params = {
-        Bucket: "switcha-production", // The path to the directory you want to upload the object to, starting with your Space name.
-        Key: "folder-path/hello-world.txt", // Object key, referenced whenever you want to access this file later.
-        Body: "Hello, Worldzzz!", // The object's contents. This variable is an object, not a string.
-        ACL: "private", // Defines ACL permissions, such as private or public.
-        Metadata: { // Defines metadata tags.
-            "x-amz-meta-my-key": "your-value"
-        }
-    };
-
-
-    // Step 4: Define a function that uploads your object using SDK's PutObjectCommand object and catches any errors.
-    const uploadObject = async () => {
-        try {
-            console.log("S3 consfig")
-            console.log({
-            endpoint: `${process.env.NEXT_PUBLIC_DO_SPACES_ENDPOINT}`, // Find your endpoint in the control panel, under Settings. Prepend "https://".
-            forcePathStyle: false,
-            region: "fra1", // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (e.g. nyc3).
-            credentials: {
-                accessKeyId: `${process.env.NEXT_PUBLIC_DO_SPACES_ID}`,
-                secretAccessKey: `${process.env.NEXT_PUBLIC_SPACES_SECRET}`,
-            }
-        })
-        console.log(s3Client)
-            const data = await s3Client.send(new PutObjectCommand(params));
-            console.log("DATA", data)
-            console.log(
-            "Successfully uploaded object: " +
-                params.Bucket +
-                "/" +
-                params.Key
-            );
-            return data;
-        } catch (err) {
-            console.log("Error", err);
-        }
-    };
-
+    
 
 
     useEffect(() => {
@@ -121,16 +83,57 @@ const Level2Verification = () => {
     const [selectedFile, setSelectedFile] = useState<any>(null)
 
     const handleFileSelected = (event: any) => {
-        console.log(event.target.files[0])
+        // console.log(event.target.files[0])
         setSelectedFile(event.target.files[0])
     }
 
     const [addLevelTwoKyc] = useAddLevelTwoKycMutation()
 
-    const handleUpload = async () => {
-        const fd = new FormData()
-        fd.append('image', selectedFile, selectedFile?.name)
-        const resp = await addLevelTwoKyc(selectedFile)
+
+    const params = {
+        Bucket: "switcha-production", // The path to the directory you want to upload the object to, starting with your Space name.
+        Key: "folder-path/hello-world.txt", // Object key, referenced whenever you want to access this file later.
+        Body: selectedFile, // The object's contents. This variable is an object, not a string.
+        ACL: "private", // Defines ACL permissions, such as private or public.
+        Metadata: { // Defines metadata tags.
+            "x-amz-meta-my-key": "your-value"
+        }
+    };
+
+
+    // Step 4: Define a function that uploads your object using SDK's PutObjectCommand object and catches any errors.
+    const uploadObject = async () => {
+        try {
+            console.log("S3 consfig")
+            console.log({
+            endpoint: `${process.env.NEXT_PUBLIC_DO_SPACES_ENDPOINT}`, // Find your endpoint in the control panel, under Settings. Prepend "https://".
+            forcePathStyle: false,
+            region: "fra1", // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (e.g. nyc3).
+            credentials: {
+                accessKeyId: `${process.env.NEXT_PUBLIC_DO_SPACES_ID}`,
+                secretAccessKey: `${process.env.NEXT_PUBLIC_SPACES_SECRET}`,
+            }
+        })
+        console.log(s3Client)
+            const data = await s3Client.send(new PutObjectCommand(params));
+            console.log("DATA", data)
+            console.log(
+            "Successfully uploaded object: " +
+                params.Bucket +
+                "/" +
+                params.Key
+            );
+            return data;
+        } catch (err) {
+            console.log("Error", err);
+        }
+    };
+
+    const handleUpload = () => {
+        // const fd = new FormData()
+        // fd.append('image', selectedFile, selectedFile?.name)
+        // const resp = await addLevelTwoKyc(fd)
+        uploadObject()
 
 
     }
