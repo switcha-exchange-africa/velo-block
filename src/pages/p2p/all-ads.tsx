e3fbe54ca73323a106649df699783cc6b827a6bb
otinomz <awesomeotinomz@gmail.com>
2022-11-26 22:58:19 +0100

    chore: mobile values

--- src/pages/p2p/all-ads.tsx
+++ src/pages/p2p/all-ads.tsx
@@ -242,7 +242,7 @@ export const RenderOrderComponent = ({ data }: any) => {
 
                                         <Td pl="0" fontSize="14px" color="#000000" pt="0" pb="0" fontWeight="600" >
                                             <Flex direction="column" height="100px"  justifyContent="flex-start" alignItems="flex-start">
-                                                <Text mb="11px">{ad?.ad[0]?.totalAmount?.toLocaleString()}</Text>
+                                                <Text mb="11px">{ad?.ad[0]?.totalAmount?.toLocaleString()} {ad?.ad[0]?.cash}</Text>
                                                 <Text mb="11px">{ad?.quantity.toLocaleString()}</Text>
                                                 <Text mb="11px">{ad?.ad[0]?.minLimit.toLocaleString()} - {ad?.ad[0]?.maxLimit.toLocaleString()} {ad?.ad[0]?.coin == 'USDT_TRON' ? 'USDT-TRON' : ad?.ad[0]?.coin}</Text>
                                             </Flex>
@@ -317,7 +317,7 @@ export const RenderOrderComponent = ({ data }: any) => {
                             <Text>Price</Text>
 
                             <Flex direction="column" mt="20px"  height="100px" fontSize="14px" color="#000000" fontWeight="600"  alignItems="flex-end" justifyContent="center">
-                                <Text mb="8px">{ad?.ad[0]?.price.toLocaleString()}</Text>
+                                <Text mb="8px">{ad?.ad[0]?.price.toLocaleString()} {ad?.ad[0]?.cash}</Text>
                                 <Text mb="8px">--</Text>
                             </Flex>
                         </Flex>
@@ -331,7 +331,7 @@ export const RenderOrderComponent = ({ data }: any) => {
                             <Text mb="11px">Limit</Text>
 
                             <Flex direction="column"  height="100px" fontSize="14px" color="#000000" fontWeight="600"  alignItems="flex-start">
-                                <Text mb="8x">{ad?.ad[0]?.totalAmount?.toLocaleString()}</Text>
+                                <Text mb="8x">{ad?.ad[0]?.totalAmount?.toLocaleString()} {ad?.ad[0]?.cash}</Text>
                                 <Text mb="8px">{ad?.quantity.toLocaleString()}</Text>
                                 <Text mb="8px">{ad?.ad[0]?.minLimit.toLocaleString()} - {ad?.ad[0]?.maxLimit.toLocaleString()} {ad?.ad[0]?.coin == 'USDT_TRON' ? 'USDT-TRON' : ad?.ad[0]?.coin}</Text>
                             </Flex>
