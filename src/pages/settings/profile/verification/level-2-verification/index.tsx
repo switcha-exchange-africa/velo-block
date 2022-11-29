cc107941d2bfb630bf914641ab015e9f41126f2b
GoodnessEzeokafor <gootech442@gmail.com>
2022-11-29 03:48:40 +0100

    fixes

--- src/pages/settings/profile/verification/level-2-verification/index.tsx
+++ src/pages/settings/profile/verification/level-2-verification/index.tsx
@@ -44,7 +44,7 @@ const Level2Verification = () => {
     const uploadObject = async () => {
         const params = {
             Bucket: `switcha-production`, // The path to the directory you want to upload the object to, starting with your Space name.
-            Key: uuid(), // Object key, referenced whenever you want to access this file later.
+            Key: `${uuid()}.png`, // Object key, referenced whenever you want to access this file later.
             Body: idImage, // The object's contents. This variable is an object, not a string.
             ACL: "public-read", // Defines ACL permissions, such as private or public.
             ContentType: "image/png",
