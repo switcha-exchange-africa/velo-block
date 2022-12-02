2b0116fa48e51d1132c6da31cc7487b1b5565d64
otinomz <awesomeotinomz@gmail.com>
2022-12-02 14:00:53 +0100

    chore: code cleanup

--- src/pages/p2p/order/index.tsx
+++ src/pages/p2p/order/index.tsx
@@ -8,21 +8,17 @@ import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { checkValidToken } from '../../../helpers/functions/checkValidToken'
 import DashboardLayout from '../../../layouts/dashboard/DashboardLayout'
-import Ads from "../../../../public/assets/svgs/ads.svg"
-import Orders from "../../../../public/assets/svgs/orders.svg"
 import More from "../../../../public/assets/svgs/more.svg"
 import UnselectedAds from "../../../../public/assets/svgs/unselectedAds.svg"
 import SelectedOrders from "../../../../public/assets/svgs/selectedOrders.svg"
 import SelectedMore from "../../../../public/assets/svgs/selectedMenu.svg"
-import mobileOrders from "../../../../public/assets/svgs/mobileOrders.svg"
-import unselectedMobileOrders from "../../../../public/assets/svgs/unselectedmobileads.svg"
 import mobileMore from "../../../../public/assets/svgs/mobileMore.svg"
 import P2pOrders from '../../../components/p2p/order/P2pOrders'
 
 
 const Order = () => {
     const router = useRouter()
-    const [tabIndex, setTabIndex] = useState(
+    const [tabIndex, setTabIndex] = useState(1)
 
     const handleTabIndex = (index: number) => {
         setTabIndex(index)
@@ -36,28 +32,16 @@ const Order = () => {
         <DashboardLayout title='Orders'>
             <Tabs defaultIndex={tabIndex} variant="unstyled" >
                 <TabList  justifyContent="flex-end" gap="63px" pr="250px" display={{ base: "none", md: "flex" }} alignItems="center" position="fixed" bg="black" color="white" zIndex="40" left="200px" py="15px" top="60px" w="100%">
-                    <Tab _selected={{color: "#FB5E04",  bg: "transparent"}} onClick={handleAdsRoute} fontWeight="700" fontSize={{ base: "16px", md: "20px" }}  p="0">
+                    <Tab onClick={handleAdsRoute} fontWeight="700" fontSize={{ base: "16px", md: "20px" }}  p="0">
                         <Flex alignItems="center" >
-                            {tabIndex === 0 ? (
-                                <Image height="20px" width="20px" src={Ads} alt="ads icon" />
-                            )
-                             : (
                                 <Image height="20px" width="20px" src={UnselectedAds} alt="ads icon" />
-                            )}
                             <Text ml="5px">My Ads</Text>
                         </Flex>
                     </Tab>
-                    <Tab _selected={{color: "#FB5E04", bg: "transparent"}} onClick={() => handleTabIndex(1)}  fontWeight="700" fontSize={{ base: "16px", md: "20px" }} p="0">
+                    <Tab   fontWeight="700" fontSize={{ base: "16px", md: "20px" }} p="0">
                         <Flex alignItems="center">
-                            {tabIndex === 1 ? (
                                 <Image height="20px" width="20px" src={SelectedOrders} alt="orders icon" />
-                            
-                            )
-                            : (
-                                <Image height="20px" width="20px" src={Orders} alt="orders icon" />
-                            )}
-                        
-                            <Text ml="5px" cursor="pointer" > Orders</Text>
+                            <Text ml="5px" cursor="pointer" color="#FB5E04" > Orders</Text>
                         </Flex>
                     </Tab>
                     <Tab isDisabled _selected={{color: "#FB5E04", bg: "transparent"}} fontWeight="700" onClick={() => handleTabIndex(2)} fontSize={{ base: "16px", md: "20px" }} p="0">
@@ -80,29 +64,16 @@ const Order = () => {
                     </Flex>
 
                     <Flex w="100%" justifyContent="space-between">
-                        <Tab _selected={{color: "#FB5E04", bg: "transparent"}} onClick={handleAdsRoute} color="black" fontWeight="700"  fontSize={{ base: "16px", md: "20px" }} p="0">
+                        <Tab  onClick={handleAdsRoute} color="black" fontWeight="700"  fontSize={{ base: "16px", md: "20px" }} p="0">
                             <Flex alignItems="center" mt="30px">
-                                {tabIndex === 0 ? (
-                                    <Image height="20px" width="20px" src={Ads} alt="ads icon" />
-                                )
-                                : (
-                                    <Image height="20px" width="20px" src={unselectedMobileOrders} alt="ads icon" />
-                                )}
-                                
-                                <Text ml="5px" >My Ads</Text>
+                                <Image height="20px" width="20px" src={UnselectedAds} alt="ads icon" />
+                                <Text ml="5px">My Ads</Text>
                             </Flex>
                         </Tab>
-                        <Tab ml="5px" _selected={{color: "#FB5E04", bg: "transparent"}} color="black" onClick={() => handleTabIndex(1)} fontWeight="700"  fontSize={{ base: "16px", md: "20px" }} p="0">
+                        <Tab ml="5px"  color="black" fontWeight="700"  fontSize={{ base: "16px", md: "20px" }} p="0">
                             <Flex alignItems="center" mt="30px">
-                                {tabIndex === 1 ? (
-                                    <Image height="20px" width="20px" src={SelectedOrders} alt="orders icon" />
-                                
-                                )
-                                : (
-                                    <Image height="20px" width="20px" src={mobileOrders} alt="orders icon" />
-                                )}
-                                
-                                <Text ml="5px" cursor="pointer" > Orders</Text>
+                                <Image height="20px" width="20px" src={SelectedOrders} alt="orders icon" />
+                                <Text ml="5px" cursor="pointer" color= "#FB5E04"> Orders</Text>
                             </Flex>
                         </Tab>
                         <Tab ml="5px" isDisabled _selected={{color: "#FB5E04", bg: "transparent"}}  color="black" fontWeight="700" onClick={() => handleTabIndex(2)} fontSize={{ base: "16px", md: "20px" }} p="0">
@@ -118,33 +89,25 @@ const Order = () => {
                 </TabList>
 
 
-                {/* <TabPanels> */}
-                    {tabIndex=== 1 ? (
-                        //Orders
-                        <>
-                            <Button
-                                onClick={() => router.back()}
-                                leftIcon={<ArrowBackIcon />}
-                                colorScheme="transparent"
-                                variant="solid"
-                                pl={0}
-                                ml="35px"
-                                mt="20px"
-                                py={"3rem"}
-                                color={'black'}
-                                display={{ base: 'none', md: 'block' }}
-                                mb={{base: "100px", md: "30px"}}
-                            >
-                                Back
-                            </Button>
-                            <Box  mt={{base: "100px", md: "30px"}}>
-                                <P2pOrders/>
-                            </Box>    
-                            
-                        </>
-
-                    ): "" }
-
+                <Button
+                    onClick={() => router.back()}
+                    leftIcon={<ArrowBackIcon />}
+                    colorScheme="transparent"
+                    variant="solid"
+                    pl={0}
+                    ml="35px"
+                    mt="20px"
+                    py={"3rem"}
+                    color={'black'}
+                    display={{ base: 'none', md: 'block' }}
+                    mb={{base: "100px", md: "30px"}}
+                >
+                    Back
+                </Button>
+                <Box  mt={{base: "100px", md: "30px"}}>
+                    <P2pOrders/>
+                </Box>    
+                
 
             </Tabs>
             
