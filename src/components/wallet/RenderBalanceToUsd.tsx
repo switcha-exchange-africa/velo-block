39eb733db7e103cfe88c03e2e23956bd6cfa09a5
otinomz <awesomeotinomz@gmail.com>
2022-12-14 12:14:31 +0100

    fix: undefined destination amount for wallet conversion

--- src/components/wallet/RenderBalanceToUsd.tsx
+++ src/components/wallet/RenderBalanceToUsd.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import _ from "lodash"
 import { Text } from '@chakra-ui/react'
 import { useLazySwapConvertQuery } from '../../redux/services/new-conversion.service'
@@ -13,7 +12,7 @@ const RenderBalanceToUsd = ({ balance, coin, variant }: RenderBalanceToUsdProps)
     const [convertCoins] = useLazySwapConvertQuery()
     const [convertedValue, setConvertedValue] = React.useState(0)
 
-    
+
     const to8Dp = (number: any) => {
         if (number > 1) {
             const datas = _.floor(number, 8)
@@ -32,7 +31,7 @@ const RenderBalanceToUsd = ({ balance, coin, variant }: RenderBalanceToUsdProps)
 
         const convertBalance = async () => {
             const convert = await convertCoins({ amount: balance, source: coin, destination: 'USDC' }).unwrap()
-            setConvertedValue(convert?.data?.destinationAmount?.destinationAmount)
+            setConvertedValue(convert?.data?.destinationAmount)
         }
         if (coin != 'USDC' || coin != 'USDT' || coin != 'USDT_TRON' || coin != 'USDT-TRON') {
             convertBalance()
@@ -49,7 +48,6 @@ const RenderBalanceToUsd = ({ balance, coin, variant }: RenderBalanceToUsdProps)
 
                 <Text color={"#64748B"} fontSize={{ md: "sm", base: 'xs' }}>
                     = {balance == 0 ? '$0.00' : coin == 'USDC' || coin == 'USDT' || coin == 'USDT_TRON' || coin == 'USDT-TRON' ? '$' + to8Dp(balance) : '$' + to8Dp(convertedValue)}
-                    {/* $200.33 */}
                 </Text>
 
             )}
