51e5a066c974aed67b5ead6a754775080270c7bf
otinomz <awesomeotinomz@gmail.com>
2022-12-21 21:49:56 +0100

    fix: number type changed to text

--- src/pages/settings/profile/verification/bank-accounts/add-bank/index.tsx
+++ src/pages/settings/profile/verification/bank-accounts/add-bank/index.tsx
@@ -159,7 +159,7 @@ const AddBankAccounts = () => {
                                             {({ field, form }: any) => (
                                                 <FormControl  pt='4' isInvalid={form.errors.accountNumber && form.touched.accountNumber}>
                                                     <FormLabel>Account Number</FormLabel>
-                                                    <Input {...field} type="number" placeholder="215xxxxx900"/>
+                                                    <Input {...field} type="text" placeholder="215xxxxx900"/>
                                                     <FormErrorMessage>{form.errors.accountNumber}</FormErrorMessage>
                                                 </FormControl>
                                             )}
