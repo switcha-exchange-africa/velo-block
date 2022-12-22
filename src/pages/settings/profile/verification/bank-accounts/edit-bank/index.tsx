dde4e3f6e1ce23111fc15574868ffea4808c4b62
otinomz <awesomeotinomz@gmail.com>
2022-12-22 10:39:28 +0100

    chore: code cleanup

--- src/pages/settings/profile/verification/bank-accounts/edit-bank/index.tsx
+++ src/pages/settings/profile/verification/bank-accounts/edit-bank/index.tsx
@@ -22,8 +22,6 @@ const AddBankAccounts = () => {
     const fetchAllUsersBank = useGetUsersBankQuery()    
     const [load, setLoad] = useState(false)
 
-    console.log("this is the name ", name)
-
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         setLoad(true)
@@ -110,99 +108,6 @@ const AddBankAccounts = () => {
                         justifyContent={"space-between"}
                         p={"20px"}
                     >
-{/*                        
-                        <Formik
-                            initialValues={{name: "", accountName: "", accountNumber: "", code: "" }}
-
-                            onSubmit={async (values:any) => {                                
-                                let res = values.name
-                                let filteredBank =  getBanks?.filter(function(bank:any) {
-                                    return bank.bankName === res;
-                                });
-                                let codeValue = filteredBank.map((code: any) => code?.bankCode)
-
-                                let newItem = codeValue[0]
-
-
-                                const data = {
-                                    ...values,
-                                    accountNumber: values.accountNumber.toString(),
-                                    code: newItem
-                                }
-                                const response:any = await addBank(data)
-                                if (response?.data?.status == 200 || response?.data?.status == 201 ) {
-                                    appAlert.success(response?.data?.message)
-                                    fetchAllUsersBank.refetch()
-                                    Router.back()
-                                } else {
-                                        appAlert.error(response?.error?.data?.message)
-                                    } 
-                                }}
-                            validateOnChange
-                            validateOnBlur
-                            validateOnMount
-                        >
-                            {({
-                                isSubmitting,
-                            }) => (
-                                <Form  >
-                                    <VStack w={{ lg: '100%', md: '100%', base: '100%' }} align='start'>
-                                        
-                                        <Field name="name" id="name" validate={validateBankName}>
-                                            {({ field , form}: any) => (
-                                            <FormControl isInvalid={form.errors.name && form.touched.name}>
-                                                <FormLabel>Bank</FormLabel>
-                                                    <Select
-                                                        {...field}  
-                                                        
-                                                    placeholder='Select Bank' cursor="pointer" iconSize={"10px"} icon={<TriangleDownIcon/>}            
-                                                >
-                                                    {getBanks?.map((item: any, index: number) => (
-                                                        <option key={index} value={item?.bankName}>{item?.bankName}</option>
-                                                    ))}
-                                                </Select>
-                                                <FormErrorMessage>{form.errors.name}</FormErrorMessage>    
-                                            </FormControl>
-                                            )}
-                                        </Field>
-                                        
-                                        <Field name='accountNumber' validate={validateAccountNumber}>
-                                            {({ field, form }: any) => (
-                                                <FormControl  pt='4' isInvalid={form.errors.accountNumber && form.touched.accountNumber}>
-                                                    <FormLabel>Account Number</FormLabel>
-                                                    <Input {...field} defaultValue={accountInfo?.accountNumber} type="text" placeholder="215xxxxx900"/>
-                                                    <FormErrorMessage>{form.errors.accountNumber}</FormErrorMessage>
-                                                </FormControl>
-                                            )}
-                                        </Field>
-
-                                        <Field name='accountName' validate={validateAccountName}>
-                                            {({ field, form }: any) => (
-                                                <FormControl  pt='4' isInvalid={form.errors.accountName && form.touched.accountName}>
-                                                    <FormLabel>Account Name</FormLabel>
-                                                    <Input {...field} defaultValue={accountInfo?.accountName} type="text" placeholder="John Doe"/>
-                                                    <FormErrorMessage>{form.errors.accountName}</FormErrorMessage>
-                                                </FormControl>
-                                            )}
-                                        </Field>
-                                                
-
-
-                                    </VStack>
-                                    <Button mt="24px" isLoading={isSubmitting} type="submit" p={"11px 22px"} color="white" bg="#FB5E04" cursor={"pointer"} borderRadius={"5px"} >
-                                        <AddIcon
-                                            mr="5px"
-                                            color={"white"}
-                                            w={"10px"}
-                                            h={"10px"}
-                                        />
-                                        Add Bank
-                                    </Button>
-
-                                </Form>
-                            )}
-
-                        </Formik> */}
                         <form onSubmit={handleSubmit}>
 
                             <FormControl>
